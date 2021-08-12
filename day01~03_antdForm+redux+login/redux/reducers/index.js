5123defd79e4c10545a29a2d08977ee9ce2d400f
LiuXiaoGuang <920396302@qq.com>
2021-08-13 00:08:08 +0800

    'myprojtct'

--- day01~03_antdForm+redux+login/redux/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux'
-import loginReducer from './login_reducer'
-export default combineReducers({
-  //该对象里的key决定着store里保存该状态的key
-  //该对象里的value决定着store里保存该状态的value
-  userInfo:loginReducer
-})
